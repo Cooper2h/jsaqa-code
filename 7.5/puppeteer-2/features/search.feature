f292abd3ac68603e9d8ffec9e96840daed8e15f9
Cooper2h <step.stepstep@yandex.ru>
2025-07-03 22:18:56 +0300

    new commit

--- 7.5/puppeteer-2/features/search.feature
+++ 7.5/puppeteer-2/features/search.feature
@@ -1,5 +1,26 @@
-Feature: Search a course
-    Scenario: Should search by text
-        Given user is on "/navigation" page
-        When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+Feature: Reserving tickets in "IdyomVKino"
+    Scenario: Should reserve one ticket
+        Given user is on page "http://qamid.tmweb.ru/client/index.php"
+        When user choose date
+        When user choose movie time
+        When user choose a seat
+        When user click on the reserve button
+        When user click on the get code button
+        Then user get the code and text "Электронный билет"
+
+        Scenario: Should reserve ticket and get a code
+        Given user is on page "http://qamid.tmweb.ru/client/index.php"
+        When user choose date
+        When user choose movie time
+        When user choose another seat
+        When user choose one more seat
+        When user click on the reserve button
+        When user click on the get code button
+        Then user get the code and text "Электронный билет"
+
+        Scenario: Should not be available button
+        Given user is on page "http://qamid.tmweb.ru/client/index.php"
+        When user choose date
+        When user choose movie time
+        When user choose already selected seat
+        Then button for reserving is inactive "true"
--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -2,7 +2,10 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { clickElement, getText } = require("../../lib/commands.js");
+const {setDefaultTimeout} = require("cucumber");
+
+setDefaultTimeout(5 * 1000);
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -17,18 +20,61 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+Given("user is on page {string}", async function (string) {
+  return await this.page.goto(`${string}`, {
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("user choose date", async function () {
+  return await clickElement(this.page, "a:nth-child(4)");
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
+When("user choose movie time", async function () {
+  return await clickElement(this.page, ".movie-seances__time[href='#'][data-seance-id='217']");
+});
+
+When("user choose a seat", async function () {
+  return await clickElement(this.page, ".buying-scheme__row > span:nth-child(5)");
+});
+
+When("user click on the reserve button", async function () {
+  return await clickElement(this.page, "button.acceptin-button");
+});
+
+When("user choose another seat", async function () {
+  return await clickElement(this.page, "div:nth-child(2) span:nth-child(4)");
+});
+
+When("user choose one more seat", async function () {
+  return await clickElement(this.page, "div:nth-child(3) span:nth-child(5)");
+});
+
+When("user click on the get code button", async function () {
+  return await clickElement(this.page, "button.acceptin-button");
+});
+
+When("user choose already selected seat", async function () {
+  return await clickElement(this.page, ".buying-scheme__chair_taken");
+});
+
+// Then("user sees the payment page and text {string}", async function (string) {
+//   const actual = await getText(this.page, "h2.ticket__check-title");
+//   const expected = await string;
+//   expect(actual).contains(expected);
+// });
+
+Then("user get the code and text {string}", async function (string) {
+  const actual = await getText(this.page, "h2.ticket__check-title");
   const expected = await string;
+  expect(actual).contains(expected);
+});
+
+Then("button for reserving is inactive {string}", async function (string) {
+  const actual = String(
+    await this.page.$eval("button", (button) => {
+      return button.disabled;
+    })
+  );
+  const expected = "true";
   expect(actual).contains(expected);
 });
