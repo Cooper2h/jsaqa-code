c40b7b8b334cbf9ba420122900f0a134c839e5a6
Cooper2h <step.stepstep@yandex.ru>
2025-07-03 18:04:25 +0300

    new commit

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,25 +9,77 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector('h1');
-    const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
   });
 
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$(".h1-mktg.col-md-10.mx-auto.mb-3");
+    await firstLink.click();
+    await page.waitForSelector("h1", { timeout: 15000 });
+    await page.waitForTimeout(2000);
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
+  }, 15000);
+
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a[href='#start-of-content']", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 10000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
-      visible: true,
-    });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    await page.waitForSelector(btnSelector, { visible: true });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent.trim());
+    expect(actual).toContain("Get started with Team");
+  }, 10000);
+
+  test("The h1 header content' (duplicate)", async () => {
+    const firstLink = await page.$(".h1-mktg.col-md-10.mx-auto.mb-3");
+    await firstLink.click();
+    await page.waitForSelector("h1", { timeout: 15000 });
+    await page.waitForTimeout(2000);
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
+  }, 15000);
+
+  test("The first link attribute (duplicate)", async () => {
+    const actual = await page.$eval("a[href='#start-of-content']", (link) => link.getAttribute("href"));
+    expect(actual).toEqual("#start-of-content");
+  }, 10000);
+
+  test("The page contains Sign in button (duplicate)", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, { visible: true });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent.trim());
+    expect(actual).toContain("Get started with Team");
+  }, 10000);
+});
+
+describe("Second task - add 3 new tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
   });
+
+  afterEach(() => {
+    page.close();
+  });
+
+  test("Blog", async () => {
+    await page.goto("https://github.blog");
+    const title = await page.title();
+    expect(title).toContain("The GitHub Blog");
+  }, 10000);
+
+  test("GitHub Security", async () => {
+    await page.goto("https://github.com/features/security");
+    const title = await page.title();
+    expect(title).toContain("GitHub Advanced Security");
+  }, 10000);
+
+  test("Check Pricing page", async () => {
+    await page.goto("https://github.com/pricing");
+    const title = await page.title();
+    expect(title).toContain("Pricing");
+  }, 10000);
 });
